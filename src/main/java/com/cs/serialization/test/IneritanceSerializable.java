88c4151ea2704dca55ff0e48fa0e63ac74b51eab
abhaypratap122 <abhaypratap@contentsphere.com>
2019-01-18 01:01:11 +0530

    Case 2: Inehritance Serializable

--- src/main/java/com/cs/serialization/test/IneritanceSerializable.java
+++ src/main/java/com/cs/serialization/test/IneritanceSerializable.java
@@ -6,13 +6,17 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
-class ParentSerialize implements Serializable {
+import com.cs.test.inheritance.Animals;
+
+class ParentSerialize {
   
   int i = 10;
 }
 
-class ChildSerialize extends ParentSerialize {
+class ChildSerialize extends ParentSerialize implements Serializable {
   
+  // It won't allow because class is not serializable
+  // ParentSerialize p = new ParentSerialize();
   int j = 20;
 }
 
@@ -21,6 +25,8 @@ public class IneritanceSerializable {
   public static void main(String[] args) throws Exception
   {
     ChildSerialize childSerialize = new ChildSerialize();
+    childSerialize.i = 50;
+    childSerialize.j = 60;
     System.out.println(childSerialize.i + " " + childSerialize.j);
     FileOutputStream fileOutputStream = new FileOutputStream(
         "D:/Abhay/Private/test/serialization.ser");
@@ -32,6 +38,8 @@ public class IneritanceSerializable {
         "D:/Abhay/Private/test/serialization.ser");
     ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
     ChildSerialize readObject = (ChildSerialize) objectInputStream.readObject();
+   
+    // INSTANCE CONTROL FLOW WILL WORK HERE
     System.out.println(readObject.i + " " + readObject.j);
     
   }
